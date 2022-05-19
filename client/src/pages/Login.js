96bc4a9c448b694c5bba9d9026cb9f18064d1dd7
YellowFlash2012 <cyclop9@hotmail.com>
2022-05-19 06:57:43 +0100

    login and signup pages UI completed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,46 @@
+import { Row, Col, Form, Input } from "antd"
+import { Link } from "react-router-dom";
+import "./Login.css"
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+      <div className="login">
+          <Row gutter={16}>
+              <Col lg={16} className="d-flex align-items-center">
+                  <img
+                      src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+                      alt=""
+                  />
+              </Col>
+
+              <Col lg={8} className="p-5">
+                  <Form layout="vertical" className="login-form p-5">
+                      <h1>Login</h1>
+                      <Form.Item
+                          name="email"
+                          label="Email"
+                          rules={[{ required: true }]}
+                      >
+                          <Input />
+                      </Form.Item>
+                      <Form.Item
+                          name="password"
+                          label="Password"
+                          rules={[{ required: true }]}
+                      >
+                          <Input />
+                      </Form.Item>
+
+                      <button className="login-btn">Login</button>
+
+                      <p style={{color:"white"}}>
+                          Don't have an account yet?{" "}
+                          <Link to="/register">Register</Link> instead
+                      </p>
+                  </Form>
+              </Col>
+          </Row>
+      </div>
+  );
+};
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,59 @@
+import { Row, Col, Form, Input } from "antd";
+import {Link} from "react-router-dom"
+import "./Login.css";
+
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
-export default Register
+      <div className="login">
+          <Row gutter={16}>
+              <Col lg={16} className="d-flex align-items-center">
+                  <img
+                      src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+                      alt=""
+                  />
+              </Col>
+
+              <Col lg={8} className="p-5">
+                  <Form layout="vertical" className="login-form p-5">
+                      <h1>Register</h1>
+                      <Form.Item
+                          name="username"
+                          label="Username"
+                          rules={[{ required: true }]}
+                      >
+                          <Input />
+                      </Form.Item>
+                      <Form.Item
+                          name="email"
+                          label="Email"
+                          rules={[{ required: true }]}
+                      >
+                          <Input />
+                      </Form.Item>
+                      <Form.Item
+                          name="password"
+                          label="Password"
+                          rules={[{ required: true }]}
+                      >
+                          <Input />
+                      </Form.Item>
+                      <Form.Item
+                          name="cpassword"
+                          label="Confirm Password"
+                          rules={[{ required: true }]}
+                      >
+                          <Input />
+                      </Form.Item>
+
+                      <button className="login-btn">Register</button>
+                      <p style={{ color: "white" }}>
+                          Already have an account?{" "}
+                          <Link to="/login">Login</Link> instead
+                      </p>
+                  </Form>
+              </Col>
+          </Row>
+      </div>
+  );
+};
+export default Register;
