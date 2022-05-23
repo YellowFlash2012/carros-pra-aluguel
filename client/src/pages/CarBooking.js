7be0ae7d0de9680587f9bf27b8fa6c3215d71e3b
YellowFlash2012 <cyclop9@hotmail.com>
2022-05-23 05:58:46 +0100

    filtered cars according to available time slots, created the stripe payment process in the carBooking page

--- client/src/pages/CarBooking.js
+++ client/src/pages/CarBooking.js
@@ -6,6 +6,7 @@ import { bookACar } from "../features/bookingSlice";
 import { Spin, Row, Col, Divider, DatePicker, Checkbox } from "antd";
 import { Navigate, useParams } from "react-router-dom";
 import moment from "moment"
+import StripeCheckout from "react-stripe-checkout";
 
 import DefaultLayout from "../components/DefaultLayout";
 
@@ -64,7 +65,49 @@ const CarBooking = () => {
 
     console.log(car);
 
+    const range = (start, end) => {
+        const result = [];
+        for (let i = start; i < end; i++) {
+            result.push(i);
+        }
+        return result;
+    }
 
+    const rangeOfDate = (start, end) => {
+
+        const dates = [];
+
+        let currentDate = start;
+        const addDays = function (days) {
+            const date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date;
+        };
+        while (currentDate <= end) {
+            dates.push(currentDate);
+            currentDate = addDays.call(currentDate, 1);
+        }
+        return dates;
+
+        
+    }
+
+    let start;
+    let end;
+
+    // car.bookedTimeSlots.map((element) => {
+    //     start = element.from;
+    //     end = element.to;
+        
+    //     console.log(start, end);
+        
+    //     const dates = rangeOfDate(new Date(start), new Date(end));
+        
+    //     dates.forEach((date) => {
+    //         console.log(date);
+    //     })
+    // });
+    
     const selectTimeSlots = (values) => {
         console.log(moment(values[0]).format("MMM DD yyy HH:mm"));
         console.log(moment(values[1]).format("MMM DD yyy HH:mm"));
@@ -89,64 +132,99 @@ const CarBooking = () => {
         dispatch(bookACar(reqObj))
     }
 
+    const onToken=(token) => {
+        console.log(token);
+        const reqObj = {
+            token,
+            user: JSON.parse(localStorage.getItem("user"))._id,
+            car: car._id,
+            totalHours,
+            totalDue,
+            driverRequired: driverNeeded,
+            bookedTimeSlots: { from, to },
+        };
+
+        dispatch(bookACar(reqObj));
+    }
+
     return (
         <DefaultLayout>
             {loading && <Spin className="spinner" size="large" />}
-            {!loading && car && <Row
-                justify="center"
-                className="d-flex align-items-center"
-                style={{ minHeight: "90vh" }}
-            >
-                <Col lg={10} sm={24} xs={24}>
-                    <img
-                        src={car.image}
-                        alt={car.name}
-                        className="car-to-book-img bs1"
-                    />
-                </Col>
-
-                <Col
-                    lg={10}
-                    sm={24}
-                    xs={24}
-                    className="text-right"
-                    style={{ textAlign: "right" }}
+            {!loading && car && (
+                <Row
+                    justify="center"
+                    className="d-flex align-items-center"
+                    style={{ minHeight: "90vh" }}
                 >
-                    <Divider dashed>Car Info</Divider>
+                    <Col lg={10} sm={24} xs={24}>
+                        <img
+                            src={car.image}
+                            alt={car.name}
+                            className="car-to-book-img bs1"
+                        />
+                    </Col>
 
-                    <div style={{ textAlign: "right" }}>
-                        <p>{car.name}</p>
-                        <p>{car.rentPerHour} Rent Per Hour /-</p>
-                        <p>Fuel Type: {car.fuelType}</p>
-                        <p>Capacity: {car.capacity}</p>
-                    </div>
+                    <Col
+                        lg={10}
+                        sm={24}
+                        xs={24}
+                        className="text-right"
+                        style={{ textAlign: "right" }}
+                    >
+                        <Divider dashed>Car Info</Divider>
 
-                    <Divider dashed>Time Slots</Divider>
+                        <div style={{ textAlign: "right" }}>
+                            <p>{car.name}</p>
+                            <p>{car.rentPerHour} Rent Per Hour /-</p>
+                            <p>Fuel Type: {car.fuelType}</p>
+                            <p>Capacity: {car.capacity}</p>
+                        </div>
 
-                    <RangePicker
-                        showTime={{ format: "HH:mm" }}
-                        format="MMM DD yyyy HH:mm"
-                        onChange={selectTimeSlots}
-                    />
+                        <Divider dashed>Time Slots</Divider>
 
-                    <div>
-                        <p>
-                            Total Hours: <b>{totalHours}</b>
-                        </p>
-                        <p>
-                            Rent Per Hour: <b>{car.rentPerHour}</b>
-                        </p>
+                        <RangePicker
+                            showTime={{ format: "HH:mm" }}
+                            format="MMM DD yyyy HH:mm"
+                            onChange={selectTimeSlots}
+                        />
 
-                        <Checkbox onChange={(e) => (e.target.checked ? setDriverNeeded(true) : setDriverNeeded(false))}>Driver Needed</Checkbox>
-                        
-                        <h2>
-                            Total Due: $<b>{totalDue}</b>
-                        </h2>
+                        {from && to && (
+                            <div>
+                                <p>
+                                    Total Hours: <b>{totalHours}</b>
+                                </p>
+                                <p>
+                                    Rent Per Hour: <b>{car.rentPerHour}</b>
+                                </p>
 
-                        <button className="booking-btn" onClick={carBookingHandler}>Book Now</button>
-                    </div>
-                </Col>
-            </Row>}
+                                <Checkbox
+                                    onChange={(e) =>
+                                        e.target.checked
+                                            ? setDriverNeeded(true)
+                                            : setDriverNeeded(false)
+                                    }
+                                >
+                                    Driver Needed
+                                </Checkbox>
+
+                                <h2>
+                                    Total Due: $<b>{totalDue}</b>
+                                </h2>
+
+                                <StripeCheckout
+                                    token={onToken}
+                                    amount={totalDue * 100}
+                                    stripeKey="process.env.REACT_APP_STRIPE_PUBLIC_KEY"
+                                >
+                                    <button className="booking-btn">
+                                        Book Now
+                                    </button>
+                                </StripeCheckout>
+                            </div>
+                        )}
+                    </Col>
+                </Row>
+            )}
         </DefaultLayout>
     );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,70 @@
 import DefaultLayout from "../components/DefaultLayout";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchAllCars } from "../features/carsSlice";
 
-import { Button, Col, Row, Spin } from "antd";
+import { Button, Col, Row, Spin, DatePicker, Checkbox } from "antd";
 import { Link } from "react-router-dom";
+import moment from "moment";
+
+const { RangePicker } = DatePicker;
 
 const Home = () => {
     const { loading, cars, error } = useSelector((store) => store.cars);
     const dispatch = useDispatch();
 
+    const [availableCars, setAvailableCars] = useState([]);
+
     useEffect(() => {
         dispatch(fetchAllCars());
     }, [dispatch]);
 
+    useEffect(() => {
+        setAvailableCars(cars)
+    },[cars])
+
+    const filterCars = (values) => {
+        let from = moment(values[0], "MMM DD yyy HH:mm");
+        let to = moment(values[1], "MMM DD yyy HH:mm");
+
+        let temp = [];
+
+        for (let car of cars) {
+            if (car.bookedTimeSlots.length===0) {
+                temp.push(car);
+            } else {
+                for (let booking of car.bookedTimeSlots) {
+                    if (
+                        from.isBetween(booking.from, booking.to) ||
+                        to.isBetween(booking.from, booking.to) || moment(booking.from).isBetween(from,to) || moment(booking.to).isBetween(from,to)
+                    ) {
+
+                    } else {
+                        temp.push(car);
+                    }
+                }
+            }
+        }
+
+        setAvailableCars(temp)
+    }
+
     return (
         <DefaultLayout>
+            <Row className="mt-3" justify="center">
+                <Col lg={20} sm={24} className="d-flex justify-content-left">
+                    <RangePicker
+                        showTime={{ format: "HH:mm" }}
+                        format="MMM DD yyyy HH:mm"
+                        onChange={filterCars}
+                    />
+                </Col>
+            </Row>
             {loading && <Spin className="spinner" size="large" />}
 
-            {!loading && cars.length ? (
+            {!loading && availableCars.length ? (
                 <Row justify="center" gutter={16}>
-                    {cars.map((car) => (
+                    {availableCars.map((car) => (
                         <Col lg={5} sm={24} xs={24} key={car._id}>
                             <div className="car p-2 bs1">
                                 <img
@@ -37,7 +81,9 @@ const Home = () => {
 
                                     <div>
                                         <button className="booking-btn me-2">
-                                            <Link to={`/booking/${car._id}`}>Book Now</Link>
+                                            <Link to={`/booking/${car._id}`}>
+                                                Book Now
+                                            </Link>
                                         </button>
                                     </div>
                                 </div>
