f081cddcb609b1a58f095f77e159bba76113bfb3
YellowFlash2012 <cyclop9@hotmail.com>
2022-06-11 08:16:42 +0100

    heroku built without issue this time but is NOT pointing to client/build/index.html to render the front end

--- server.js
+++ server.js
@@ -21,22 +21,23 @@ if (process.env.NODE_ENV === "development") {
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res)=> {
-    res.send("We are live and ready!")
-})
-
-app.use("/api/v1/cars", carsRoutes)
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/bookings", bookingRoutes)
 
 // config for deployment
 if (process.env.NODE_ENV="production") {
     app.use("/", express.static("client/build"));
-
+    
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client/build/index.html"));
     });
 }
+
+// app.get("/", (req, res)=> {
+//     res.send("We are live and ready!")
+// })
+
+app.use("/api/v1/cars", carsRoutes)
+app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/bookings", bookingRoutes)
 
 connectDB()
 app.listen(port, () => {
